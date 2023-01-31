44ef61284c32324dcff92d479630f8796522cb34
wangzhizhou <824219521@qq.com>
2023-01-31 13:25:12 +0800

    添加位置测试文件

--- LocationReminder/Views/MapPage.swift
+++ LocationReminder/Views/MapPage.swift
@@ -45,50 +45,50 @@ struct MapPage: View {
             .frame(width: 44, height: 44)
             .padding(EdgeInsets(top: 0, leading: 0, bottom: 30, trailing: 5))
         }
-//        .overlay(alignment: .topTrailing, content: {
-//            List {
-//                Picker("配置", selection: $appModel.mapType) {
-//                    Text("标准").tag(MapType.standard)
-//                    Text("混合").tag(MapType.hybrid)
-//                    Text("图像").tag(MapType.imagery)
-//                }
-//            }.frame(height: 50)
-//        })
-//        .overlay(alignment: .bottomLeading) {
-//            Button {
-////                appModel.triggerNotification("test")
-//                appModel.showCurrentUserLocation()
-//            } label: {
-//                Text("Test")
-//                    .padding(EdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5))
-//            }
-//            .padding()
-//        }
-//        .overlay(alignment: .topLeading) {
-//            VStack {
-//
-//                Button {
-//                    if appModel.userTrackModel == .follow {
-//                        appModel.userTrackModel = .followWithHeading
-//                    } else {
-//                        appModel.userTrackModel = .follow
-//                    }
-//                } label: {
-//                    Image(systemName: appModel.userTrackModel == .follow ? "location.circle.fill" : "location.circle")
-//                        .resizable()
-//                        .frame(width: 30, height: 30)
-//                        .padding(EdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5))
-//                }
-//                .padding()
-//
-//                Spacer()
-//
-//            }
-//            .frame(width: 50, height: 150)
-//            .background(.white.opacity(0.4))
-//            .clipShape(RoundedRectangle(cornerRadius: 10))
-//            .padding(EdgeInsets(top: 44, leading: 5, bottom: 0, trailing: 0))
-//        }
+        //        .overlay(alignment: .topTrailing, content: {
+        //            List {
+        //                Picker("配置", selection: $appModel.mapType) {
+        //                    Text("标准").tag(MapType.standard)
+        //                    Text("混合").tag(MapType.hybrid)
+        //                    Text("图像").tag(MapType.imagery)
+        //                }
+        //            }.frame(height: 50)
+        //        })
+        .overlay(alignment: .bottomLeading) {
+            Button {
+                //                appModel.triggerNotification("test")
+                appModel.showCurrentUserLocation()
+            } label: {
+                Text("Test")
+                    .padding(EdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5))
+            }
+            .padding()
+        }
+        //        .overlay(alignment: .topLeading) {
+        //            VStack {
+        //
+        //                Button {
+        //                    if appModel.userTrackModel == .follow {
+        //                        appModel.userTrackModel = .followWithHeading
+        //                    } else {
+        //                        appModel.userTrackModel = .follow
+        //                    }
+        //                } label: {
+        //                    Image(systemName: appModel.userTrackModel == .follow ? "location.circle.fill" : "location.circle")
+        //                        .resizable()
+        //                        .frame(width: 30, height: 30)
+        //                        .padding(EdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5))
+        //                }
+        //                .padding()
+        //
+        //                Spacer()
+        //
+        //            }
+        //            .frame(width: 50, height: 150)
+        //            .background(.white.opacity(0.4))
+        //            .clipShape(RoundedRectangle(cornerRadius: 10))
+        //            .padding(EdgeInsets(top: 44, leading: 5, bottom: 0, trailing: 0))
+        //        }
     }
 }
 
